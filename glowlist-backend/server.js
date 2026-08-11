3295a1aadad12b09b2f004f9f1daa3053b44986f
Muh_Lathif_K <mlathifk@gmail.com>
2026-08-11 16:35:58 +0700

    create

---
+++ glowlist-backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database Glowlist');
+    }
+});
+const PORT = 3001;
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Selamat Datang di Glowlist API sudah berjalan');
+});
+
+app.get('/produk', (req,res) => {
+    const sql = 'Select * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req,res) => {
+    const sql = 'Select * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server  Glowlist jalan di http://localhost:${PORT}`);
+});
